5892cd0f9ba0d46bcb46d0776c3c4e720c4190e1
owner-PC\owner <newsams2@gmail.com>
2018-05-29 00:09:09 +0100

    Adding Client Project

---
+++ Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Configuration;
+
+namespace Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Service");
+                string connStr = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+
+                // Listen for a session based message.
+                QueueClient qClientResponse = QueueClient.CreateFromConnectionString(connStr, "MyQueueRequest");
+                qClientResponse.OnMessage(message =>
+                {
+                    Console.WriteLine("I've Received a Message.");
+
+                    Console.WriteLine($"Message ID: {message.MessageId} - {message.ReplyToSessionId}");
+
+                    var receivedPayload = message.GetBody<Messages.Message>();
+                    var sessionToRelyTo = message.ReplyToSessionId;
+
+                    Console.WriteLine($"Message from client - {receivedPayload.MyMessage}");
+
+                    var responsePayload = new Messages.Message();
+                    receivedPayload.MyMessage = "Bye bye world!!!!!!!";
+                    BrokeredMessage bm = new BrokeredMessage(receivedPayload);
+                    bm.SessionId = sessionToRelyTo;
+
+
+                    QueueClient qClient = QueueClient.CreateFromConnectionString(connStr, "MyQueueResponse");
+                    qClient.Send(bm);
+                    Console.WriteLine("Sent Message!");
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+namespace Messages
+{
+    [Serializable]
+    public class Message
+    {
+        public string MyMessage { get; set; }
+    }
+    [Serializable]
+    public class Response
+    {
+        public string MyMessage { get; set; }
+    }
+}
--- Service/Program.cs
+++ Service/Program.cs
@@ -1,15 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Configuration;
 
-namespace Service
+namespace Client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string connStr = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+
+            Console.WriteLine("Client");
+            try
+            {
+
+                var payload = new Messages.Message();
+                payload.MyMessage = "Hello world";
+
+                var messageId = Guid.NewGuid().ToString();
+                Console.WriteLine($"My Mesasge ID is {messageId}");
+                BrokeredMessage bm = new BrokeredMessage(payload);
+                bm.ReplyToSessionId = messageId;
+
+                NamespaceManager ns = NamespaceManager.CreateFromConnectionString(connStr);
+
+
+                if (!ns.QueueExists("MyQueueRequest"))
+                {
+                    QueueDescription queue = new QueueDescription("MyQueueRequest");
+                    queue.EnablePartitioning = false;
+                    queue.RequiresSession = false;
+
+                    ns.CreateQueue(queue);
+                }
+                if (!ns.QueueExists("MyQueueResponse"))
+                {
+                    QueueDescription queue = new QueueDescription("MyQueueResponse");
+                    queue.EnablePartitioning = false;
+                    queue.RequiresSession = true;
+
+                    ns.CreateQueue(queue);
+                }
+
+                // Sending to a queue
+                QueueClient qClient = QueueClient.CreateFromConnectionString(connStr, "MyQueueRequest");
+                qClient.Send(bm);
+                Console.WriteLine("Sent Message!");
+
+
+                // Listen for a session based message.
+                QueueClient qClientResponse = QueueClient.CreateFromConnectionString(connStr, "MyQueueResponse");
+                var messageSession = qClientResponse.AcceptMessageSession(messageId);
+                messageSession.OnMessage(message =>
+                {
+                    Console.WriteLine("I've Received a Message.");
+
+                    Console.WriteLine($"Message ID: {message.MessageId} - {message.SessionId}");
+
+                    var receivedPayload = message.GetBody<Messages.Message>();
+
+                    Console.WriteLine($"Message from service - {receivedPayload.MyMessage}");
+
+                },
+                new OnMessageOptions());
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
         }
+    }
+
+}
+
+namespace Messages
+{
+    [Serializable]
+    public class Message
+    {
+        public string MyMessage { get; set; }
     }
 }
